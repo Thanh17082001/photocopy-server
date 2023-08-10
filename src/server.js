e39c93a0e68983be3c5c9ba0e3c603d5b77061a3
Thanh17082001 <thienthanh17082001@gmail.com>
2023-08-10 21:25:45 +0700

    Khoi tao du an

---
+++ src/server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+import session  from 'express-session'
+const app = express()
+import dotenv from 'dotenv'
+dotenv.config()
+const port= 3000
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`   )
+
+})
