1b2fdc36d99e14543a24f3a39680c54e2403662e
Thanh17082001 <thienthanh17082001@gmail.com>
2023-08-17 20:51:47 +0700

    crud product

--- src/controller/product.controller.js
+++ src/controller/product.controller.js
@@ -1,114 +1,111 @@
-import productService from "../service/product.service";
-import deepEqual from "deep-equal";
-class productController{
-    async create(req, res){
+import productService from '../service/product.service';
+import deepEqual from 'deep-equal';
+class productController {
+    async create(req, res) {
         try {
-            if(!!req.body){
-                const image= !!req.file ? req.file.path.split('public')[1].replace(/\\/g, '/') : '';
-                const data={
+            if (!!req.body) {
+                const image = !!req.file ? req.file.path.split('public')[1].replace(/\\/g, '/') : '';
+                const data = {
                     brandId: req.body.brandId,
                     name: req.body.name,
                     inputQuantity: req.body.inputQuantity,
                     priceSale: req.body.priceSale,
                     priceRental: req.body.priceRental,
                     description: req.body.description,
-                    image:image,
-                    type: req.body.type  
-                }
-                const existProduct= await productService.checkExistNameAndBrandId(data.name, data.brandId)
-                if(!!existProduct){
-                    res.json({mes:'Sản phẩm đã tồn tại hãy chỉnh sửa sản phẩm', status:false})
+                    image: image,
+                    type: req.body.type,
+                };
+                const existProduct = await productService.checkExistNameAndBrandId(data.name, data.brandId);
+                if (!!existProduct) {
+                    res.json({ mes: 'Sản phẩm đã tồn tại hãy chỉnh sửa sản phẩm', status: false });
                     return;
                 }
-                await productService.create(data)
-                res.json({mes:'Thêm sản phẩm thành công', status:true})
-            }else{
-                res.json({mes:"Dữ liệu trống", status:false})
+                await productService.create(data);
+                res.json({ mes: 'Thêm sản phẩm thành công', status: true });
+            } else {
+                res.json({ mes: 'Dữ liệu trống', status: false });
             }
-
         } catch (error) {
-            res.status(500).json({error})
+            res.status(500).json({ error });
         }
     }
 
     // read
-    async getAllProduct(req, res){
+    async getAllProduct(req, res) {
         try {
-            const {pageNumber, pageSize} = req.query
-            const result = await productService.findProduct({}, pageNumber, pageSize)
-            res.json(result)
+            const { pageNumber, pageSize } = req.query;
+            const result = await productService.findProduct({}, pageNumber, pageSize);
+            res.json(result);
         } catch (error) {
-            res.status(500).json({error})
+            res.status(500).json({ error });
         }
     }
 
-    async getProductById(req, res){
+    async getProductById(req, res) {
         try {
-            const {id}= !!req.query ? req.query :''
-            const result = await productService.findProduct(id)
-            res.json(result)
+            const { id } = !!req.query ? req.query : '';
+            const result = await productService.findProduct(id);
+            res.json(result);
         } catch (error) {
-            res.status(500).json({error})
+            res.status(500).json({ error });
         }
     }
 
-    async getProductByBrandId(req, res){
+    async getProductByBrandId(req, res) {
         try {
-            const {brandId} = req.query
-            const result = await productService.findProduct({brandId})
-            res.json(result)
+            const { brandId } = req.query;
+            const result = await productService.findProduct({ brandId });
+            res.json(result);
         } catch (error) {
-            res.status(500).json({error})
+            res.status(500).json({ error });
         }
     }
 
     //update
-    async update(req, res){
+    async update(req, res) {
         try {
-            const {id}= !!req.query ? req.query :''
-            const product= await productService.findProductById(id)
-            const oldProduct={
-                brandId:product.brandId.toString(),
-                name:product.name,
-                inputQuantity:product.inputQuantity,
-                priceSale:product.priceSale,
-                priceRental:product.priceRental,
-                description:product.description,
-                image:product.image,
-                type:product.type 
-            }
-            const image= !!req.file ? req.file.path.split('public')[1].replace(/\\/g, '/') : product.image;
-            if(!!req.body){
-                const newProduct={
+            const { id } = !!req.query ? req.query : '';
+            const product = await productService.findProductById(id);
+            const oldProduct = {
+                brandId: product.brandId.toString(),
+                name: product.name,
+                inputQuantity: product.inputQuantity,
+                priceSale: product.priceSale,
+                priceRental: product.priceRental,
+                description: product.description,
+                image: product.image,
+                type: product.type,
+            };
+            const image = !!req.file ? req.file.path.split('public')[1].replace(/\\/g, '/') : product.image;
+            if (!!req.body) {
+                const newProduct = {
                     ...req.body,
-                    image
+                    image,
+                };
+                const isEqual = deepEqual(oldProduct, newProduct);
+                if (isEqual) {
+                    res.json({ mes: 'Chưa chỉnh sửa dữ liệu', status: false });
+                } else {
+                    await productService.updateProduct(id, newProduct);
+                    res.json({ mes: 'Cập nhật thành công', status: true });
                 }
-                const isEqual= deepEqual(oldProduct,newProduct)
-                if(isEqual){
-                    res.json({mes:"Chưa chỉnh sửa dữ liệu", status:false})
-                }
-                else{
-                    await productService.updateProduct(id,newProduct)
-                    res.json({mes:'Cập nhật thành công',status:true})
-                }
-            }
-            else{
-                res.json({mes:'Dữ liệu không được bỏ trống', status:false})
+            } else {
+                res.json({ mes: 'Dữ liệu không được bỏ trống', status: false });
             }
         } catch (error) {
-            res.status(500).json({error})
+            res.status(500).json({ error });
         }
     }
 
-    async delete(req, res){
+    async delete(req, res) {
         try {
-            const {id} = req.query
-            const result = await productService.deleteProduct(id)
-            res.json(result)
+            const { id } = req.query;
+            const result = await productService.deleteProduct(id);
+            res.json(result);
         } catch (error) {
-            res.status(500).json({error})
+            res.status(500).json({ error });
         }
     }
 }
 
-export default new productController()
+export default new productController();
